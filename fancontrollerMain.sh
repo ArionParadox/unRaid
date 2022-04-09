e3bad591d54522b246969604e154695a9a94cb35
Arion <63235031+ArionParadox@users.noreply.github.com>
2022-04-09 22:22:45 +1000

    Update fancontrollerMain.sh

--- fancontrollerMain.sh
+++ fancontrollerMain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #This was created to control the fans in my Dell Poweredge. I am lucky enough to be on the right iDrac version to use this method. 
-#I didn't want to get my temperate from iDrac as currently some of my drives are outside the cooling shroud; hence needing to grab the data from smartctl
+#I didn't want to get my temperatures from iDrac as currently some of my drives are outside the cooling shroud; hence needing to grab the data from smartctl
 
 #Inspiration from --  https://github.com/kmwoley/unRAID-Tools/blob/master/unraid_array_fan.sh
 #and from -- https://www.reddit.com/r/unRAID/comments/guwrjy/so_you_bought_a_dell_poweredge_server_for_unraid/ftc82qi/ 
